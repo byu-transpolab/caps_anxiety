6039e26409f9bf986e001bebfab17ce88b162e13
Emily Blanchard <ekblnchrd@gmail.com>
2023-09-11 12:40:15 -0600

    add targets to check the folder of files, read in location files, add demographoc data, and add mental health responses

--- _targets.R
+++ _targets.R
@@ -18,24 +18,41 @@ tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", "gpsactivs",
                             "ggspatial", "data.table", "plotly", "future.apply",
                             "viridis", "pomp", "stats", "osmdata"))
 
-# End this file with a list of target objects.
+# List of target objects
 list(
+  
   # Read in and clean GPS data
-  tar_target(cleaned_data, cleanData("data/caps_data"), resources = tar_resources(
+  tar_target(fileslist,
+    command =
+      list.files("data/caps_data", pattern = "*.csv", full.names = TRUE)
+    ),
+  
+  tar_target(cleaned_data, process_caps_data(fileslist), resources = tar_resources(
     future = tar_resources_future(resources = list(n_cores = threads))
   )),
-  tar_target(manual_table, makeManualTable("manual_clusters20220826")),
   
-  # Join cleaned data and manual data to allow for calibration
-  tar_target(cleaned_manual_table, joinTables(manual_table, cleaned_data)),
-  
-  
-  #tar_target(maps, makeAllMaps(cleaned_data)),
-  #tar_target(optimized_sann_params, optimize(cleaned_manual_table)),
-  #tar_target(optimized_optim_params, optimize2(cleaned_manual_table)),
-  tar_target(accurate_sann_tibble, makeClusters(cleaned_manual_table = cleaned_data,
+  # Make activity clusters using the optimized parameters
+  tar_target(clustered_data, makeClusters(cleaned_manual_table = cleaned_data,
                                                 params = c(11.7,3,300,1))),
-  tar_target(num_trips, addNumTrips(accurate_sann_tibble)),
-  tar_target(activity_types, addTripType(num_trips))
-  #tar_target(final_table, addMentalHealthResponses(activity_types))
-)
+  
+  # Determine the number of trips made on a given day by a given user
+  tar_target(num_trips, addNumTrips(clustered_data)),
+  
+  # Read in location files for parks, groceries, and libraries
+  tar_target(parks_file, "data/resources/parks.geojson", format = "file"),
+  tar_target(parksSf, sf::st_read(parks_file) %>% sf::st_transform(32612)),
+  
+  tar_target(grocery_file, "data/resources/groceries.geojson", format = "file"),
+  tar_target(grocerySf, sf::st_read(grocery_file) %>% sf::st_transform(32612)),
+  
+  tar_target(library_file, "data/resources/libraries.geojson", format = "file"),
+  tar_target(librarySf, sf::st_read(library_file) %>% sf::st_transform(32612)),
+  
+  # Determine number of trips for each activity type
+  tar_target(activity_types, addTripType(num_trips, parksSf, grocerySf, librarySf)),
+  
+  # Add the demographic data
+  tar_target(demographics_table, addDemographicData(activity_types)),
+  
+  # Add the mental health responses
+  tar_target(final_table, addMentalHealthResponses(demographics_table))
