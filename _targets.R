e2dcedb1412efed0593cf5f001f8ccd5943b7d60
Emily Blanchard <ekblnchrd@gmail.com>
2023-09-11 12:37:08 -0600

    update the _targets.R file to begin data processing

--- _targets.R
+++ _targets.R
@@ -1,44 +1,6 @@
-# # Created by use_targets().
-# # Follow the comments below to fill in this target script.
-# # Then follow the manual to check and run the pipeline:
-# #   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
-# 
-# # Load packages required to define the pipeline:
-# library(targets)
-# # library(tarchetypes) # Load other packages as needed. # nolint
-# 
-# # Set target options:
-# tar_option_set(
-#   packages = c("tidyverse", "mlogit", "modelsummary"), # packages that your targets need to run
-#   format = "rds" # default storage format
-#   # Set other options as needed.
-# )
-# 
-# # tar_make_clustermq() configuration (okay to leave alone):
-# # options(clustermq.scheduler = "multicore")
-# 
-# # tar_make_future() configuration (okay to leave alone):
-# # Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
-# 
-# # Load the R scripts stored in R/ with your custom functions:
-# for (file in list.files("R", full.names = TRUE)) source(file)
-# # source("other_functions.R") # Source other scripts as needed. # nolint
-# 
-# # Replace the target list below with your own:
-# list(
-#   tar_target(
-#     name = car_mlogit,
-#     command = make_data()
-#   ),
-#   tar_target(
-#     name = models,
-#     command = estimate_models(car_mlogit)
-#   )
-# )
-
 library(targets)
 
-# parallelization
+# Parallelization
 library(future)
 library(future.apply)
 library(furrr)
@@ -47,11 +9,7 @@ plan(callr)
 threads <- future::availableCores() - 1
 
 source("R/Optimize.R")
-source("R/MakeMaps.R")
 source("R/Mental Health Table.R")
-
-tar_option_set(debug = "cleaned_manual_table")
-
 
 # Set target-specific options such as packages.
 tar_option_set(packages = c("dplyr","tools", "hms", "lubridate", 
