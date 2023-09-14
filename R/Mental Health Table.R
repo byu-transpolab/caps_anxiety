36ea24597f3dc795665ac7f059f565c9cf0ce403
Emily Blanchard <ekblnchrd@gmail.com>
2023-09-14 16:49:11 -0600

    Fixed the function to add Mental Health Data and just the columns with the user Id, data, and answers. For duplicated questions, add 'morning' and 'evening' after the appropriate questions

--- R/Mental Health Table.R
+++ R/Mental Health Table.R
@@ -47,20 +47,53 @@ add_location <- function(x, variable, sf){
   return(a)
 }
 
+# Create a vector of the survey questions that were repeated in the morning and in the evening
+morning <- read.csv("data/mental_surveys/Morning_Survey_220223.csv")
+start_column <- which(names(morning) == "Have.you.thought.about.killing.yourself.in.the.past.12.hours.or.since.you.last.took.a.survey.")
+end_column <- which(names(morning) == "Would.you.like.a.member.of.the.research.team.to.reach.out.to.you.because.you.are.feeling.unsafe.")
+selected_columns <- names(morning)[start_column:end_column]
+morning_columns <- as.vector(selected_columns)
+
+evening <- read.csv("data/mental_surveys/Evening_Survey_220223.csv")
+start_column <- which(names(evening) == "Have.you.thought.about.killing.yourself.in.the.past.12.hours.or.since.you.last.took.a.survey.")
+end_column <- which(names(evening) == "Would.you.like.a.member.of.the.research.team.to.reach.out.to.you.because.you.are.feeling.unsafe.")
+selected_columns <- names(evening)[start_column:end_column]
+evening_columns <- as.vector(selected_columns)
+
 # Add Mental Health Responses and Demographic Data
 addMentalHealthResponses <- function(tibble){
   morningResponses <- read.csv("data/mental_surveys/Morning_Survey_220223.csv") %>%
-    rename(userId = Study.Id, date = Survey.Started.Date) %>% 
-    mutate(date = dmy(date)) %>%
-    mutate(date = format(date, "%Y-%m-%d")) %>% 
-    mutate(date = as.Date(date))
-  # eveningResponses <- read.csv("data/mental_surveys/Evening_Survey_220223.csv") %>% 
-  #   rename(userId = Study.Id)
+    rename(userId = User.Id, date = Survey.Started.Date) %>% 
+    mutate(date = as.Date(format(dmy(date), "%Y-%m-%d"))) %>% 
+    select(
+      userId,
+      Survey.Name,
+      date,
+      `What.time.did.you.actually.try.to.go.to.sleep.last.night.`:`Would.you.like.a.member.of.the.research.team.to.reach.out.to.you.because.you.are.feeling.unsafe.` 
+    ) %>% 
+    # Add "morning" at the end of the column names that are repeated in the morning and in the evening
+    rename_with(~paste0(., "morning"), all_of(morning_columns))
+    
+  eveningResponses <- read.csv("data/mental_surveys/Evening_Survey_220223.csv") %>% 
+    rename(userId = User.Id, date = Survey.Started.Date) %>% 
+    mutate(date = as.Date(format(dmy(date), "%Y-%m-%d"))) %>% 
+    select(
+      userId,
+      Survey.Name,
+      date,
+      `How.would.you.gauge.your.energy.levels.in.the.past.day.`:`Would.you.like.a.member.of.the.research.team.to.reach.out.to.you.because.you.are.feeling.unsafe.` 
+    ) %>%
+    # Add "evening" at the end of the column names that are repeated in the morning and in the evening
+    rename_with(~paste0(., "evening"), all_of(evening_columns))
+    
   full_join(tibble, morningResponses, by = c('userId', 'date'))
+  full_join(tibble, eveningResponses, by = c('userId', 'date'))
 }
 
 addDemographicData <- function(tibble) {
   demographic <- readxl::read_excel("data/mental_surveys/Demographic_Breakdown.xlsx") %>%
-    rename(userId = Metricwire_ID)
+    rename(userId = Metricwire_ID) %>% 
+    select( -c(Recipient_First_Name))
+    
   full_join(tibble, demographic, by = 'userId')
 }
