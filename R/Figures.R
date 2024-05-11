bb1273042afd9bb08d1a42f8d3747e131a05ae76
Emily Blanchard <ekblnchrd@gmail.com>
2024-05-10 20:29:15 -0600

    Update this - by removing the unneeded functions

--- R/Figures.R
+++ R/Figures.R
@@ -1,122 +1,14 @@
 ## METHODS
-# Descriptive numeric statistics
-desc_num <- function(demo_ids) {
-  descriptives_num <- demo_ids %>% 
+# Descriptive statistics
+desc_stats <- function(demo_ids) {
+  descriptive_stats <- demo_ids %>% 
     filter(!is.na(userId)) %>% 
-    select(age, fsiq_2) %>% 
-    mutate(fsiq_2 = as.numeric(fsiq_2)) %>%
+    select(age, fsiq_2, prescribed_group, sex, race) %>% 
+    mutate(fsiq_2 = as.numeric(fsiq_2),
+           prescribed_group = fct_recode(prescribed_group, Control = "No Group")) %>%
     rename(Age = age,
-           IQ = fsiq_2) %>% 
-    datasummary_skim()
-}
-
-# Descriptive categorical statistics
-desc_cat <- function(demo_ids) {
-  descriptives_cat <- demo_ids %>% 
-    filter(!is.na(userId)) %>% 
-    select(prescribed_group, sex, race) %>% 
-    mutate(prescribed_group = fct_recode(prescribed_group, Control = "No Group")) %>% 
-    rename(Group = prescribed_group,
+           IQ = fsiq_2,
+           Group = prescribed_group,
            Sex = sex,
-           Race = race) %>% 
-    datasummary_skim(type = "categorical")
+           Race = race)
 }
-
-# Distribution of the number of activities engaged in
-numAct <- function(final_table) {
-  raw_trips <- final_table %>% 
-    filter(!is.na(numTrips))
-  
-  ggplot(raw_trips, aes(x = numTrips)) +
-    geom_histogram(binwidth = 1, fill = "darkgray", color = "black") +
-    # geom_text(stat = "count", aes(label = after_stat(count)),
-              # vjust = -0.5, color = "black", size = 3) +
-    labs(title = "Distribution of Number of Activities", x = "Number of Activities", y = "Frequency") +
-    theme_bw()
-}
-
-# Distribution of the 7-day rolling average number of activities
-numAct_7 <- function(final_table) {
-  trips <- final_table %>% 
-    filter(!is.na(sev_day_avg))
-  
-  ggplot(trips, aes(x = sev_day_avg)) +
-    geom_histogram(binwidth = 1, fill = "darkgray", color = "black") +
-    labs(title = "Number of Activities with Seven Day Average", x = "Number of Activities", y = "Frequency") +
-    theme_bw()
-}
-
-# Distribution of the 14-day rolling average number of activities
-numAct_14 <- function(final_table) {
-  trips <- final_table %>% 
-    filter(!is.na(fourteen_day_avg))
-  
-  ggplot(trips, aes(x = fourteen_day_avg)) +
-    geom_histogram(binwidth = 1, fill = "darkgray", color = "black") +
-    labs(title = "Number of Activities with Fourteen Day Average", x = "Number of Activities", y = "Frequency") +
-    theme_bw()
-}
-
-# Distribution of the 30-day rolling average number of activities
-numAct_30 <- function(final_table) {
-  trips <- final_table %>% 
-    filter(!is.na(thirty_day_avg))
-  
-  ggplot(trips, aes(x = thirty_day_avg)) +
-    geom_histogram(binwidth = 1, fill = "darkgray", color = "black") +
-    labs(title = "Number of Activities with Thirty Day Average", x = "Number of Activities", y = "Frequency") +
-    theme_bw()
-}
-
-# Distribution of the area covered as calculated by the convex_hull
-area_dist <- function(final_table) {
-  trips <- final_table %>% 
-    filter(!is.na(area))
-  
-  ggplot(trips, aes(x = area/(1e6))) +
-    geom_histogram(fill = "darkgray", color = "black") +
-    scale_x_log10() +
-    labs(title = "Distribution of Area Covered", x = "Area (km^2)", y = "Frequency") +
-    theme_bw()
-}
-
-# Distribution of the distance traveled 
-length_dist <- function(final_table) {
-  trips <- final_table %>% 
-    filter(!is.na(length))
-  
-  ggplot(trips, aes(x = length/1000)) +
-    geom_histogram(fill = "darkgray", color = "black") +
-    scale_x_log10() +
-    labs(title = "Distribution of Distance Traveled", x = "Length (km)", y = "Frequency") +
-    theme_bw()
-}
-
-
-## RESULTS
-# Boxplot showing the distribution of the number of activities for each neuro group
-neuro_numAct <- function(model_data) {
-  acts <- model_data %>% 
-    filter(!is.na(sev_day_avg))
-  
-  ggplot(acts, aes(x = prescribed_group, y = sev_day_avg)) +
-    geom_boxplot(fill = "darkgray", color = "black") +
-    labs(title = "Distribution of Activities based on Neuro Group",
-         x = "Neuro Group", y = "Seven Day Average Number of Activities") +
-    theme_bw()
-}
-
-# Percent suicidal ideation across the different groups
-neuro_suicide <- function(model_data) {
-  suicidal_percent <- model_data %>%
-    group_by(prescribed_group) %>%
-    summarize("Percentage of Days Suicidal" = mean(suicidal_ideation_q31_even == TRUE, na.rm = TRUE) * 100) %>% 
-    rename("Neuro Group" = prescribed_group) %>% 
-    as.data.frame()
-  
-  tt(suicidal_percent, digits = 3, theme = "bootstrap")
-}
-
-
-
-
